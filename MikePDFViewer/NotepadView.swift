9a2960603955bec40b86cd7a60c2b0b4eda10484
MyCache63 <mashe63@gmail.com>
2026-07-28 21:39:51 -0700

    Fix build break: .tooltip is not a SwiftUI modifier, use .help (blocked MC3 which links MikePDFViewerKit) [builds]

--- MikePDFViewer/NotepadView.swift
+++ MikePDFViewer/NotepadView.swift
@@ -82,7 +82,7 @@ struct NotepadView: View {
                 } label: {
                     Image(systemName: "trash")
                 }
-                .tooltip("Delete Note")
+                .help("Delete Note")
                 .disabled(selectedID == nil)
             }
         }
